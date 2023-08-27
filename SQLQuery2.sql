5224e3492255a3e0a8c9dc66863c7fdbfbe6c362
Olga_K <okobilska@gmail.com>
2023-08-27 13:00:04 +0300

    Змынено колыр кнопок у блоцы Вправи

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,4 +1,4 @@
-﻿        CREATE DATABASE English ON 
+﻿ CREATE DATABASE MyDatabase ON 
         (FILENAME = 'd:\Users\Olga\Desktop\Project.mdf'), 
         (FILENAME = 'd:\Users\Olga\Desktop\Project.ldf') 
         FOR ATTACH;
